81968961686e592f7d25b3b09ea17f2df11aaada
yanncb <elwiko1337@gmail.com>
2020-05-13 15:07:17 +0200

    ajout du fichier de config de bdd dans myerp-consumer.

--- myerp-business/src/test/java/com/dummy/myerp/business/impl/manager/ComptabiliteManagerImplTest.java
+++ myerp-business/src/test/java/com/dummy/myerp/business/impl/manager/ComptabiliteManagerImplTest.java
@@ -5,25 +5,15 @@ import com.dummy.myerp.model.bean.comptabilite.EcritureComptable;
 import com.dummy.myerp.model.bean.comptabilite.JournalComptable;
 import com.dummy.myerp.model.bean.comptabilite.LigneEcritureComptable;
 import com.dummy.myerp.technical.exception.FunctionalException;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 public class ComptabiliteManagerImplTest {
 
-    @Rule
-    public ExpectedException thrown = ExpectedException.none();
 
     private ComptabiliteManagerImpl manager = new ComptabiliteManagerImpl();
-    private List<CompteComptable> compteComptableList = new ArrayList<>();
-    private List<JournalComptable> journalComptableList = new ArrayList<>();
-    private JournalComptable journal;
-
 
     @Test
     public void checkEcritureComptableUnit() throws Exception {
@@ -55,6 +45,8 @@ public class ComptabiliteManagerImplTest {
 
     @Test(expected = FunctionalException.class)
     public void checkEcritureComptableUnitRG2() throws Exception {
+
+        // GIVEN
         EcritureComptable vEcritureComptable = new EcritureComptable();
         vEcritureComptable.setJournal(new JournalComptable("AC", "Achat"));
         vEcritureComptable.setDate(new Date());
@@ -65,13 +57,18 @@ public class ComptabiliteManagerImplTest {
         vEcritureComptable.getListLigneEcriture().add(new LigneEcritureComptable(new CompteComptable(2),
                 null, null,
                 new BigDecimal(1234)));
+        // WHEN
         manager.checkEcritureComptableUnit(vEcritureComptable);
+
+        // THEN
+        // expected FunctionalException (voir conf du test)
+
     }
 
     @Test(expected = FunctionalException.class)
     public void checkEcritureComptableUnitRG3() throws Exception {
-        EcritureComptable vEcritureComptable;
-        vEcritureComptable = new EcritureComptable();
+        EcritureComptable vEcritureComptable = new EcritureComptable();
+
         vEcritureComptable.setJournal(new JournalComptable("AC", "Achat"));
         vEcritureComptable.setDate(new Date());
         vEcritureComptable.setLibelle("Libelle");
@@ -83,5 +80,25 @@ public class ComptabiliteManagerImplTest {
                 null));
         manager.checkEcritureComptableUnit(vEcritureComptable);
     }
+
+    @Test(expected = FunctionalException.class)
+    public void checkEcritureComptableUnitRG5() throws Exception {
+        EcritureComptable vEcritureComptable = new EcritureComptable();
+
+        vEcritureComptable.setJournal(new JournalComptable("AC", "Achat"));
+        vEcritureComptable.setDate(new Date());
+        vEcritureComptable.setLibelle("Libelle");
+        vEcritureComptable.getListLigneEcriture().add(new LigneEcritureComptable(new CompteComptable(1),
+                null, new BigDecimal(10),
+                null));
+        vEcritureComptable.getListLigneEcriture().add(new LigneEcritureComptable(new CompteComptable(1),
+                null, null,
+                new BigDecimal(10)));
+
+
+        vEcritureComptable.setReference("AC-2016/00001");
+        manager.checkEcritureComptable(vEcritureComptable);
+    }
+
 
 }
